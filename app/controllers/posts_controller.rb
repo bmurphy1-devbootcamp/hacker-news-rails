be76eeb518f69926d4336b4664dba31368667b4d
Apprentice <apprentice@dbc-chicago-7.local>
2014-02-17 14:27:12 -0600

    Add destroy and update functionality to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,4 +16,19 @@ class PostsController <ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(params[:post].permit(:title, :url, :text))
+    redirect_to post
+  end
+
+  def destroy
+    Post.destroy(params[:id])
+    redirect_to root_path
+  end
 end
