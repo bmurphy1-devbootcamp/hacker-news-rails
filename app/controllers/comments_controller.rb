b4b16f9af7cb976f607cb05bfcfcce1873186543
Apprentice <apprentice@dbc-chicago-7.local>
2014-02-17 16:22:51 -0600

    Add comment creation and deletion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.new(params[:comment].permit(:text))
+    @comments.user_id = session[:current_user_id]
+    @comments.save
+    redirect_to post_path(params[:post_id])
+  end
+
+  def destroy
+    Comment.destroy(params[:id])
+    redirect_to post_path(params[:post_id])
+  end
 end
