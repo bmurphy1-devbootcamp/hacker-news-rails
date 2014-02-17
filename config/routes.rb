1a0b56beda6450c76115517611852f2927e80767
Apprentice <apprentice@dbc-chicago-7.local>
2014-02-17 13:43:30 -0600

    Create migrations, models, model associations

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ HackerNews::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :posts do
+    resources :comments
+  end
+
+  resources :users, only: [:create, :new, :update, :destroy]
+
+  get '/login' => 'sessions#login'
+
+  root 'posts#index'
+
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
